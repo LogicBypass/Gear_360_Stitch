9cf6187c1236b1d005e6c9fbdd1c8d6c0e218498
LogicBypass <62214984+LogicBypass@users.noreply.github.com>
2022-08-20 22:51:26 +0300

    photostitch script v1
    work in a loop for all files in folder

---
+++ photostitch.ps1
@@ -0,0 +1,14 @@
+$scriptpath = $MyInvocation.MyCommand.Definition 
+[string]$dir = Split-Path $scriptpath  
+set-location $dir
+
+$files = Get-ChildItem "*.JPG"
+$nr = 1
+
+foreach ($f in $files){
+    ffmpeg -f lavfi -i nullsrc=size=2896x2896 -vf "format=gray8,geq='clip(128-128/6*(180-190/(2896/2)*hypot(X-2896/2,Y-2896/2)),0,255)',v360=input=fisheye:output=e:ih_fov=195:iv_fov=194" -frames 1 -y mergePmap.png
+    ffmpeg -i $f -i mergePmap.png -lavfi "[0]crop=h=2896:y=0,format=rgb24,split[a][b];
+    [a]crop=ih:iw/2:0:0,v360=input=fisheye:output=e:ih_fov=195:iv_fov=194[c];
+    [b]crop=ih:iw/2:iw/2:0,v360=input=fisheye:output=e:yaw=180:ih_fov=195:iv_fov=194[d];[1]format=gbrp[e];[c][d]
+    [e]maskedmerge" -y out$nr.jpg
+    $nr++}
