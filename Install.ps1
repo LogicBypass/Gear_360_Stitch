bb51a17af56ffb2973c1078842dbde772c463082
LogicBypass <62214984+LogicBypass@users.noreply.github.com>
2022-09-03 23:42:21 +0300

    Update Install.ps1

--- Install.ps1
+++ Install.ps1
@@ -3,6 +3,27 @@ if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]:
     Start-Process PowerShell -Verb RunAs "-NoProfile -ExecutionPolicy Bypass -Command `"cd '$pwd'; & '$PSCommandPath';`"";
     exit;
 }
+
+
+Write-Host "888                       d8b               888888b.                                                "
+Write-Host "888                       Y8P               888  .88b                                               "
+Write-Host "888                                         888  .88P                                               "
+Write-Host "888      .d88b.   .d88b.  888  .d8888b      8888888K.  888  888 88888b.   8888b.  .d8888b  .d8888b  "
+Write-Host "888     d88..88b d88P.88b 888 d88P.         888  .Y88b 888  888 888 .88b     .88b 88K      88K      "
+Write-Host "888     888  888 888  888 888 888           888    888 888  888 888  888 .d888888 -Y8888b. -Y8888b. "
+Write-Host "888     Y88..88P Y88b 888 888 Y88b.         888   d88P Y88b 888 888 d88P 888  888      X88      X88 "
+Write-Host "88888888 -Y88P-   *Y88888 888  *Y8888P      8888888P*   *Y88888 88888P*  *Y888888  88888P*  88888P* "
+Write-Host "                      888                                   888 888                                 "
+Write-Host "                 Y8b d88P                              Y8b d88P 888                                 "
+Write-Host "                  *Y88P*                                *Y88P*  888                                 "
+Write-Host ""
+Write-Host ""
+Write-Host "Gear 360 Instalation Script"
+Write-Host "https://github.com/LogicBypass/Gear_360_Stitch"
+Write-Host ""
+Write-Host ""
+Start-Sleep 3
+
 New-Item -Type Directory -Path C:\ffmpeg ; Set-Location C:\ffmpeg
 curl.exe -L 'https://www.gyan.dev/ffmpeg/builds/ffmpeg-release-essentials.zip' -o 'ffmpeg.zip'
 # Expand the Zip
