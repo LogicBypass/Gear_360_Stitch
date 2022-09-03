95243ae04b0fb0070045f8664af2914d84214c0b
LogicBypass <62214984+LogicBypass@users.noreply.github.com>
2022-09-03 22:26:58 +0300

    Instalation script for FFmpeg

---
+++ Install.ps1
@@ -0,0 +1,32 @@
+#https://www.gyan.dev/ffmpeg/builds/ffmpeg-git-full.7z
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    Start-Process PowerShell -Verb RunAs "-NoProfile -ExecutionPolicy Bypass -Command `"cd '$pwd'; & '$PSCommandPath';`"";
+    exit;
+}
+New-Item -Type Directory -Path C:\ffmpeg ; Set-Location C:\ffmpeg
+curl.exe -L 'https://www.gyan.dev/ffmpeg/builds/ffmpeg-release-essentials.zip' -o 'ffmpeg.zip'
+# Expand the Zip
+Expand-Archive .\ffmpeg.zip -Force -Verbose
+# Move the executable (*.exe) files to the top folder
+Get-ChildItem -Recurse `
+    -Path C:\ffmpeg\ffmpeg\ffmpeg-* -Filter *.* | ForEach-Object {Move-Item $_ -Destination C:\ffmpeg -Verbose}
+# Clean up
+Remove-Item .\ffmpeg\ffmpeg* -Recurse
+Remove-Item .\ffmpeg.zip
+# List the directory contents:
+#Get-ChildItem
+
+setx /m PATH "C:\ffmpeg\bin;%PATH%"
+
+Write-Host "Restart your PC to complete instalation"
+
+$input = Read-Host "Restart computer now [Y/n]"
+while("y","n" -notcontains $YesOrNo ){
+    $input = Read-Host "Restart computer now [Y/n]"
+    switch($input){
+              y{Restart-computer -Force -Confirm:$false}
+              n{exit}
+        default{write-warning "Invalid Input"}}
+}
+
+#https://adamtheautomator.com/install-ffmpeg/
