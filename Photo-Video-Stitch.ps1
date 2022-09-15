41cf84dec9c348a0381e2d53737626d196c7e179
LogicBypass <62214984+LogicBypass@users.noreply.github.com>
2022-09-15 16:52:01 +0300

    Script will preserve original filenames

--- Photo-Video-Stitch.ps1
+++ Photo-Video-Stitch.ps1
@@ -34,19 +34,20 @@ $nr = 1
 
 foreach ($f in $files){
     $ext = [IO.Path]::GetExtension($f)
+    $out=(Get-Item $f ).Basename
     if ($ext -eq ".jpg" ){
         <#                                H   H                                       C      FOV    H               H       H                                                  FOV        FOV #>       
         ffmpeg -f lavfi -i nullsrc=size=2896x2896 -vf "format=gray8,geq='clip(128-128/8*(180-195/(2896/2)*hypot(X-2896/2,Y-2896/2)),0,255)',v360=input=fisheye:output=e:ih_fov=195:iv_fov=194" -frames 1 -y mergePmap.png
         ffmpeg -i $f -i mergePmap.png -lavfi "[0]crop=h=2896:y=0,format=rgb24,split[L][R];
         [L]crop=ih:iw/2:0:0,v360=input=fisheye:output=e:ih_fov=195:iv_fov=194[L_fov];
         [R]crop=ih:iw/2:iw/2:0,v360=input=fisheye:output=e:yaw=180:ih_fov=195:iv_fov=194[R_fov];[1]format=gbrp[fmt];
-        [L_fov][R_fov][fmt]maskedmerge" -y out$nr.jpg
+        [L_fov][R_fov][fmt]maskedmerge" -y $out'_STITCHED'.jpg
         $nr++}
     else {
         ffmpeg -f lavfi -i nullsrc=size=2048x2048 -vf "format=gray8,geq='clip(128-128/8*(180-195/(2048/2)*hypot(X-2048/2,Y-2048/2)),0,255)',v360=input=fisheye:output=e:ih_fov=195:iv_fov=194" -frames 1 -y mergeVmap.png
         ffmpeg -hwaccel auto -i $f -i mergeVmap.png  -f lavfi -i color=black:s=2x2 -lavfi "[0]format=rgb24,split[L][R];
         [L]crop=ih:iw/2:0:0,v360=input=fisheye:output=e:ih_fov=195:iv_fov=194.2[L_fov];
         [R]crop=ih:iw/2:iw/2:0,v360=fisheye:e:yaw=180:ih_fov=195:iv_fov=194.2[R_fov];
-        [1]format=gbrp[fmt];[L_fov][R_fov][fmt]maskedmerge,overlay=shortest=1" -qp 13 -b:v 30M -b:a 192k -r 24 -y out$nr.MP4
+        [1]format=gbrp[fmt];[L_fov][R_fov][fmt]maskedmerge,overlay=shortest=1" -qp 13 -b:v 30M -b:a 192k -r 24 -y $out'_STITCHED'.MP4
         $nr++}
     }
