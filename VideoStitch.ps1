b633bfd830653ef1faa66c4d452f838169fa6f32
LogicBypass <62214984+LogicBypass@users.noreply.github.com>
2022-09-21 19:12:32 +0300

    Fixing difference between lenses FOV
    FOV ARE NOT EQUAL for Left and Right lenses, Left lens capture more info so FOV is less than on the Right lens

--- VideoStitch.ps1
+++ VideoStitch.ps1
@@ -1,10 +1,14 @@
 <# 
 C - Width of interpolation band in degrees, must be smaller or equal than FOV "try from 6 to 11 for smoother stitch edge" 
 H - Half of the image width = height of input image after cropping
+
 FOV - field of view of the fisheye lenses in degrees, "try to play with it from 190 to 199, Be sure to change all 7 of them!" 
+FOV May not be the same for horizontal(ih_fov) / vertical position(iv_fov)
+FOV ARE NOT EQUAL for Left and Right lenses, Left lens capture more info so FOV is less than on the Right lens
 
 For debuging edges use "bgr24" instad of "gray8"
 #>
+
 
 
 Write-Host "888                       d8b               888888b.                                                "
@@ -30,16 +34,15 @@ $scriptpath = $MyInvocation.MyCommand.Definition
 [string]$dir = Split-Path $scriptpath  
 set-location $dir
 
-$files = Get-ChildItem "360*.MP4"
-$nr = 1
+$files = Get-ChildItem "360*[0-9].MP4"
 
 <#                                H   H                                       C      FOV    H               H       H                                                  FOV        FOV #>       
-ffmpeg -f lavfi -i nullsrc=size=2048x2048 -vf "format=gray8,geq='clip(128-128/8*(180-195/(2048/2)*hypot(X-2048/2,Y-2048/2)),0,255)',v360=input=fisheye:output=e:ih_fov=195:iv_fov=194" -frames 1 -y mergeVmap.png
+ffmpeg -f lavfi -i nullsrc=size=2048x2048 -vf "format=gray8,geq='clip(128-128/7*(180-195/(2048/2)*hypot(X-2048/2,Y-2048/2)),0,255)',v360=input=fisheye:output=e:ih_fov=195:iv_fov=195" -frames 1 -y mergeVmap.png
 
 foreach ($f in $files){
     $out=(Get-Item $f ).Basename
     ffmpeg -hwaccel auto -i $f -i mergeVmap.png  -f lavfi -i color=black:s=2x2 -lavfi "[0]format=rgb24,split[L][R];
-    [L]crop=ih:iw/2:0:0,v360=input=fisheye:output=e:ih_fov=195:iv_fov=194.2[L_fov];
-    [R]crop=ih:iw/2:iw/2:0,v360=fisheye:e:yaw=180:ih_fov=195:iv_fov=194.2[R_fov];
+    [L]crop=ih:iw/2:0:0,v360=input=fisheye:output=e:ih_fov=193:iv_fov=192[L_fov];
+    [R]crop=ih:iw/2:iw/2:0,v360=fisheye:e:yaw=180:ih_fov=195:iv_fov=194[R_fov];
     [1]format=gbrp[fmt];[L_fov][R_fov][fmt]maskedmerge,overlay=shortest=1" -qp 13 -b:v 30M -b:a 192k -r 24 -y V.$out'_STITCHED'.MP4
-    $nr++}
+    }
